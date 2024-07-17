18b8a100c14680161776a589e2212ccf036d8438
Eduardo Silva <thiago.novak7@gmail.com>
2024-07-17 09:30:14 -0300

    get

--- src/main/java/com/gerenciadordetarefas/gerenciadordetarefas/controller/MinhaPrimeiraController.java
+++ src/main/java/com/gerenciadordetarefas/gerenciadordetarefas/controller/MinhaPrimeiraController.java
@@ -1,5 +1,13 @@
 package com.gerenciadordetarefas.gerenciadordetarefas.controller;
 
+
+//@Controller // Pra quando tenha páginas...
+@RestController // O NOME JÁ DIZ TUDO.... API
+@RequestMapping("/primeiraRota")
 public class MinhaPrimeiraController {
     
+    @GetMapping("/")
+    public String primeiraMensagem() {
+        return "Funcionouuu";
+    }
 }
