9ddb9cac0ff387bc8a9cf1ba2ddd33e5d3696b8f
Eduardo Silva <thiago.novak7@gmail.com>
2024-07-19 14:55:56 -0300

    config user
    config user, class

--- src/main/java/com/gerenciadordetarefas/gerenciadordetarefas/user/UserController.java
+++ src/main/java/com/gerenciadordetarefas/gerenciadordetarefas/user/UserController.java
@@ -1,4 +1,16 @@
+package com.gerenciadordetarefas.gerenciadordetarefas.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+
+
+    public void create() {
+
+    }
     
 }
