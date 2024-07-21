1e6773dbab6c58772bcc58af3460639f4b838021
eduardonk9999 <thiago.novak7@gmail.com>
2024-07-21 10:35:23 -0300

    get and seters

--- src/main/java/com/gerenciadordetarefas/gerenciadordetarefas/user/UserModel.java
+++ src/main/java/com/gerenciadordetarefas/gerenciadordetarefas/user/UserModel.java
@@ -4,4 +4,34 @@ public class UserModel {
     private String userName;
     private String name;
     private String password;
+
+    /**
+     * Get aara pegar infos
+     * Set para setar infos
+     * Coom nossas classes estão com private, é dessa forma q faremos para manipular as informações
+     * */
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
