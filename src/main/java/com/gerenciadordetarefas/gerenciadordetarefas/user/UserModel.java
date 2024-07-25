2615be56c49e528d5b35583fff3854d076ab6bf6
eduardonk9999 <thiago.novak7@gmail.com>
2024-07-25 10:37:21 -0300

    feat: tornando user unico

--- src/main/java/com/gerenciadordetarefas/gerenciadordetarefas/user/UserModel.java
+++ src/main/java/com/gerenciadordetarefas/gerenciadordetarefas/user/UserModel.java
@@ -1,5 +1,6 @@
 package com.gerenciadordetarefas.gerenciadordetarefas.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -18,6 +19,7 @@ public class UserModel {
     @GeneratedValue(generator = "UUID")
     private UUID id;
 
+    @Column(unique = true)
     private String userName;
     private String name;
     private String password;
